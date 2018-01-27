dee7db632fb5a2ad1798442ace360799b23a3b08
Luke McDowall <lukemcdowall@gmail.com>
2018-01-27 16:10:51 +1300

    detergent

--- Assets/Scripts/ChemistryManager.cs
+++ Assets/Scripts/ChemistryManager.cs
@@ -5,196 +5,145 @@ using UnityEngine.UI;
 
 public class ChemistryManager : MonoBehaviour
 {
-    static int round = 3;
+    [Range(1, 11)]
+    public int round = 3;
+
+    [Range(1, 10)]
+    public float countdown = 3.0f;
     public List<InputField> Chemicals = new List<InputField>();
-    public InputField R;
-    public InputField O1;
-    public InputField O2;
-    public InputField O3;
-    public InputField HN;
-    public InputField N;
-    public InputField H;
-    public InputField S;
-    public InputField OH;
-    public InputField CH31;
-    public InputField CH32;
+    public List<InputField> otherChem = new List<InputField>();
+    //public InputField R;
+    //public InputField O1;
+    //public InputField O2;
+    //public InputField O3;
+    //public InputField HN;
+    //public InputField N;
+    //public InputField H;
+    //public InputField S;
+    //public InputField OH;
+    //public InputField CH31;
+    //public InputField CH32;
+    public Button submit;
+    public Button LookAgain;
     public Text scoreText;
     public Text countDown;
-    private float countdown = 3.0f;
     private int score = 0;
-
+    private bool review = false;
     
 
     public void Submit_Click()
     {
 
-        foreach(InputField i in Chemicals)
+        int enabled = 0;
+        foreach (InputField i in Chemicals)
         {
-            if(i.text == i.name)
+            
+            
+            string name = i.name.Split('(')[0];
+
+            
+            if (i.text == name && i.enabled == true)
             {
-                score++;
-                Debug.Log(i.text +" " + i.name);
+                    score++;
+                    i.enabled = false;
+                    Debug.Log("Score + 1");
+                
+            }
+            else if (i.text != name)
+            {
+                // i.transition = new Selectable.Transition(new Color(0,0,255));
+                // i.colors.normalColor.r = 0;// = new Color(0, 0, 255);
+                
+                ColorBlock cb = i.colors;
+                cb.normalColor = Color.red;
+                i.colors = cb;
+                score--;
+            }
+            if (i.enabled == true)
+            {
+                enabled++;
             }
         }
 
-        //if(R.text != "R")
-        //{
-        //    Debug.Log("R not " + R.text);
-        //}
-        //else if()
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
+        if (enabled == 0)
+        {
+            countDown.text = "end";
+            submit.enabled = false;
+            LookAgain.enabled = false;
+        }
 
-        //if (O1.text != "O")
-        //{
-        //    Debug.Log("O not " + O1.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (O2.text != "O")
-        //{
-        //    Debug.Log("O not " + O2.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (O3.text != "O")
-        //{
-        //    Debug.Log("O not " + O3.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (HN.text != "HN")
-        //{
-        //    Debug.Log("HN not " + HN.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (N.text != "N")
-        //{
-        //    Debug.Log("N not " + N.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (H.text != "H")
-        //{
-        //    Debug.Log("H not " + H.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (S.text != "S")
-        //{
-        //    Debug.Log("S not " + S.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (OH.text != "OH")
-        //{
-        //    Debug.Log("OH not " + OH.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (CH31.text != "CH3")
-        //{
-        //    Debug.Log("CH3 not " + CH31.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
-
-        //if (CH32.text != "CH3")
-        //{
-        //    Debug.Log("CH3 not " + CH32.text);
-        //}
-        //else
-        //{
-        //    score++;
-        //    Debug.Log("Correct");
-        //}
 
 
         scoreText.text = "Score: " + score;
     }
 
+    public void ReView()
+    {
+        score -= 3;
+        scoreText.text = "Score: " + score;
+
+        countdown = 5;
+        countDown.text = countdown.ToString();
+        foreach (InputField i in Chemicals)
+        {
+            if(i.enabled == true)
+            {
+                Debug.Log("re-view");
+                i.text = i.name;
+                otherChem.Add(i);
+            }
+        }
+        //if (countdown <= 0)
+        //{
+        //    foreach (InputField x in otherChem)
+        //    {
+        //        x.text = "";
+
+        //    }
+        //    otherChem.Clear();
+        //}
+    }
 	// Use this for initialization
 	void Start ()
     {
 
-        Chemicals.Add(R);
-        Chemicals.Add(O1);
-        Chemicals.Add(O2);
-        Chemicals.Add(O3);
-        Chemicals.Add(HN);
-        Chemicals.Add(N);
-        Chemicals.Add(H);
-        Chemicals.Add(S);
-        Chemicals.Add(OH);
-        Chemicals.Add(CH31);
-        Chemicals.Add(CH32);
-        
-
+        //Chemicals.Add(R);
+        //Chemicals.Add(O1);
+        //Chemicals.Add(O2);
+        //Chemicals.Add(O3);
+        //Chemicals.Add(HN);
+        //Chemicals.Add(N);
+        //Chemicals.Add(H);
+        //Chemicals.Add(S);
+        //Chemicals.Add(OH);
+        //Chemicals.Add(CH31);
+        //Chemicals.Add(CH32);
         
         foreach(InputField i in Chemicals)
         {
             
             i.text = i.name;
-            //R.text = "R";
-            i.readOnly = true;
+            i.enabled = false;
             
         }
-       
-
-        
-        
    }
     public void Begin()
     {
-        countDown.text = "";
         for (int i = 0; i < round; i++)
         {
             int r = Random.Range(0, Chemicals.Count);
             Chemicals[r].text = "";
             //R.text = "R";
-            Chemicals[r].readOnly = false;
+            Chemicals[r].enabled = true;
 
-            //Chemicals.Remove(Chemicals[r]);
-            
+            otherChem.Add(Chemicals[r]);
+            Chemicals.Remove(Chemicals[r]);
         }
+        foreach (InputField x in otherChem)
+        {
+            Chemicals.Add(x);
+        }
+        otherChem.Clear();
     }
 
     // Update is called once per frame
@@ -207,8 +156,21 @@ public class ChemistryManager : MonoBehaviour
                 countDown.text = countdown.ToString();
                 if (countdown <= 0)
                 {
-                
+                countDown.text = "";
+                if (review == false)
+                {
                     Begin();
+                    review = true;
+                }
+                else
+                {
+                    foreach (InputField x in otherChem)
+                    {
+                        x.text = "";
+
+                    }
+                    otherChem.Clear();
+                }
                 }
             }
        
