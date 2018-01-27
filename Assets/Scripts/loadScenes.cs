6eb52f079c2bb4dd94fc76282795581bffac203b
Luke McDowall <lukemcdowall@gmail.com>
2018-01-28 08:51:36 +1300

    Added main menu

---
+++ Assets/Scripts/loadScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScenes : MonoBehaviour {
+
+    public void loadScene()
+    {
+        //Application.LoadLevel("TowerDefence");
+        //loadScene("TowerDefence");
+        //UnityEngine.SceneManagement.loa
+        Debug.Log("Hi");
+        SceneManager.LoadScene("Scenes/TowerDefence");
+
+    }
+}
